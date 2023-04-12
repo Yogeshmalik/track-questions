4028b83d356fddda42c9ba2efda91517a025353d
Yogesh Malik <ysmworking@gmail.com>
2023-04-13 01:28:46 +0530

    7

--- src/UploadForm.js
+++ src/UploadForm.js
@@ -1,20 +1,22 @@
-// To create the form component in React, you could use a library like react-dropzone
-// or react-file-input to simplify the file upload process.
-// Here's an example of how you could create a file upload component using react-dropzone:
-// https://www.youtube.com/watch?v=YOAeBSCkArA this video shows how to uplaoad and retrieve img from firebase storage
-
-// import { storage } from "./firebaseConfig";
-// import { ref, uploadBytes, listAll } from "firebase/storage";
-// import { v4 } from "uuid";
 import React, { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import firebase from "firebase/compat/app";
 import "firebase/compat/database";
+import { dbRef, firebaseConfig } from "./firebaseConfig";
 
 const UploadForm = () => {
   const [file, setFile] = useState(null);
   const [text, setText] = useState("");
   const [error, setError] = useState(null);
+  const [userData, setUserData] = useState({
+    question: "",
+    options: { option1: "", option2: "", option3: "", option4: "" },
+    timesUsed: "",
+    timesRemaining: "",
+    totalLimit: "",
+  });
+  const [latestData, setLatestData] = useState({});
+  // const [totalLimit, setTotalLimit] = useState(100);
 
   const onDrop = useCallback((acceptedFiles) => {
     setFile(acceptedFiles[0]);
@@ -33,7 +35,19 @@ const UploadForm = () => {
     if (file) {
       questions = await parseFile(file);
     } else {
-      questions = parseText(text);
+      questions = [
+        {
+          text: userData.question.trim(),
+          options: [
+            userData.options.option1.trim(),
+            userData.options.option2.trim(),
+            userData.options.option3.trim(),
+            userData.options.option4.trim(),
+          ],
+          remainingUsage: userData.timesRemaining,
+          lastUsed: null,
+        },
+      ];
     }
     try {
       await uploadQuestions(questions);
@@ -61,46 +75,162 @@ const UploadForm = () => {
 
   const parseText = (text) => {
     const lines = text.trim().split("\n");
-    const questions = lines.map((line) => ({
-      text: line.trim(),
-      remainingUsage: 100,
-      lastUsed: null,
-    }));
+    const questions = lines.map((line) => {
+      const [questionText, ...options] = line.trim().split(",");
+      return {
+        text: questionText.trim(),
+        options: options.map((option) => option.trim()),
+        remainingUsage: 100,
+        lastUsed: null,
+      };
+    });
     return questions;
   };
 
   const uploadQuestions = async (questions) => {
-    const dbRef = firebase.database().ref("questions");
     for (const question of questions) {
       const newQuestionRef = dbRef.push();
       const newQuestion = {
-        ...question,
+        question: question.text,
+        option1: question.options[0],
+        option2: question.options[1],
+        option3: question.options[2],
+        option4: question.options[3],
+        timesUsed: question.lastUsed ? 1 : 0,
+        timesRemaining: question.remainingUsage,
+        totalLimit: 100,
         createdAt: firebase.database.ServerValue.TIMESTAMP,
       };
       await newQuestionRef.set(newQuestion);
     }
+    // Get the latest uploaded data from Firebase
+    dbRef.limitToLast(1).once("value", (snapshot) => {
+      const latest = snapshot.val();
+      if (latest) {
+        setLatestData(latest[Object.keys(latest)[0]]);
+      }
+    });
+  };
+
+  const uploadUserData = async () => {
+    try {
+      const userDataRef = dbRef.child("userData");
+      const snapshot = await userDataRef.once("value");
+      const userData = snapshot.val() || {};
+      const newUserData = {
+        ...userData,
+        [latestData.text]: {
+          option1: latestData.options[0],
+          option2: latestData.options[1],
+          option3: latestData.options[2],
+          option4: latestData.options[3],
+          timesUsed: latestData.lastUsed ? 1 : 0,
+          timesRemaining: latestData.remainingUsage,
+          totalLimit: 100,
+        },
+      };
+      await userDataRef.set(newUserData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleOptionChange = (optionNumber) => (event) => {
+    setUserData({
+      ...userData,
+      options: {
+        ...userData.options,
+        [`option${optionNumber}`]: event.target.value,
+      },
+    });
+  };
+
+  const handleNumericChange = (name) => (event) => {
+    setUserData({ ...userData, [name]: event.target.value });
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div {...getRootProps()}>
-        <input {...getInputProps()} />
-        <p className="drag-drop-select">
-          Drag and drop a file here, or click HERE to select a file
-        </p>
-      </div>
-      <textarea
-        placeholder="ENTER YOUR QUESTION HERE..."
-        className="inputarea"
-        value={text}
-        onChange={handleTextChange}
-      />
-      <button type="submit">UPLOAD file or UPLOAD Entered Question</button>
+    <>
+      <form onSubmit={handleSubmit}>
+        <div {...getRootProps()}>
+          <input {...getInputProps()} />
+          <p className="drag-drop-select">
+            Drag and drop a file here, or click HERE to select a file
+          </p>
+        </div>
+        <textarea
+          placeholder="ENTER YOUR QUESTION HERE..."
+          className="inputarea"
+          value={text}
+          onChange={handleTextChange}
+        />
+        <div className="options-container">
+          <label>
+            Option 1:
+            <input
+              type="text"
+              value={userData.options.option1}
+              onChange={handleOptionChange(1)}
+            />
+          </label>
+          <label>
+            Option 2:
+            <input
+              type="text"
+              value={userData.options.option2}
+              onChange={handleOptionChange(2)}
+            />
+          </label>
+          <label>
+            Option 3:
+            <input
+              type="text"
+              value={userData.options.option3}
+              onChange={handleOptionChange(3)}
+            />
+          </label>
+          <label>
+            Option 4:
+            <input
+              type="text"
+              value={userData.options.option4}
+              onChange={handleOptionChange(4)}
+            />
+          </label>
+        </div>
+        <div className="numeric-inputs-container">
+          <label>
+            Times used:
+            <input
+              type="number"
+              value={userData.timesUsed}
+              onChange={handleNumericChange("timesUsed")}
+            />
+          </label>
+          <label>
+            Times remaining:
+            <input
+              type="number"
+              value={userData.timesRemaining}
+              onChange={handleNumericChange("timesRemaining")}
+            />
+          </label>
+          <label>
+            Total limit:
+            <input
+              type="number"
+              value={userData.totalLimit}
+              onChange={handleNumericChange("totalLimit")}
+            />
+          </label>
+        </div>
+        <button type="submit">UPLOAD</button>
 
-      {error && <p className="errorMsg">{error}</p>}
-    </form>
+        {error && <p className="errorMsg">{error}</p>}
+      </form>
+      {/* <MyComponent timesUsed={timesUsed} timesRemaining={timesRemaining} totalLimit={totalLimit} /> */}
+    </>
   );
 };
 
 export default UploadForm;
-
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,25 +1,30 @@
-import firebase from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import { getStorage } from "firebase/storage";
+import { initializeApp } from "firebase/app";
+import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 import "firebase/compat/database";
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBX8KTobRdoK4B5bmn65PutFXrMI0-CL2s",
   authDomain: "track-questions.firebaseapp.com",
-  databaseURL: "https://track-questions-default-rtdb.asia-southeast1.firebasedatabase.app",
+  databaseURL:
+    "https://track-questions-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "track-questions",
   storageBucket: "track-questions.appspot.com",
   messagingSenderId: "645079332384",
   appId: "1:645079332384:web:417e36e1bc06aebb41ae45",
   measurementId: "G-RMBMP4NXP2",
 };
+firebase.initializeApp(firebaseConfig)
+const dbRef = firebase.database().ref("questionRecords");
 
+export { dbRef, firebaseConfig };
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export const storage = getStorage(app);
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// export const storage = getStorage(app);
 
 // const db = firebase.firestore();
 // const database = firebase.database();
+// export default firebaseConfig;
